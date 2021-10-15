2cc6c1d570190b1cc9cfc47d0ca85066d11a2082
Fabrizio Spataro <fabrizio.spataro@agilelab.it>
2021-10-15 16:52:38 +0200

    WIP home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,46 +6,51 @@ import axios from 'axios';
 
 import { bannerImg } from '../../products'
 
-let products = [];
 
-function Home() {
-    setTimeout(() => {
+class Home extends React.Component {
+    
+    constructor(props) {
+      super(props);
+
+      this.state = {products: []}
+
         //NO AUTH
-        axios.get(config.SERVER + config.API + config.REST.PRODUCT_LIST).then(function (response) {
+        axios.get(config.SERVER + config.API + config.REST.PRODUCT_LIST).then((response) => {
             console.log(response);
-            products = response.data;
-            this.setState(state => ({ loaded: true})); 
+            this.setState({products : response.data});
         })
         .catch(function (error) {
             console.log(error);
         })
-    }, 1000);
+    }
 
-    return (
-        <div className="home">
-            <img className="home__image" src={bannerImg} alt=""/>
-            
-            {/*product id, title, price, rating */}
+    render() {
+        
+        return (
+            <div className="home">
+                <img className="home__image" src={bannerImg} alt=""/>
+                
+                {/*product id, title, price, rating */}
 
-            <div className="home__row">
-                {products.map(item => {
-                    return (
-                        <Product
-                            code={item.code}
-                            title={item.title}
-                            image={item.image}
-                            price={item.price}
-                            rating={item.rating}
-                        />
+                <div className="home__row">
+                    {this.state.products.map(item => {
+                        return (
+                            <Product
+                                code={item.code}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
 
-                    )
-                })}
+                        )
+                    })}
+
+                </div>
 
             </div>
-
-        </div>
-
-    )
+        )
+    }
 }
 
 export default Home
